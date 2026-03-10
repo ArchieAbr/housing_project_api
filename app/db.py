fe2759ab52614a5ccdcd1e616893450fbd22c0a4
ArchieAbr <archiepabraham@gmail.com>
2026-03-10 13:54:02 +0000

    setup SQLAlchemy database engin and session factory

---
+++ app/db.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Fetch the database URL from the environment variables set in docker-compose.yml
+# fallback string just in case it runs outside of Docker during testing
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://user:password@db:5432/housing_db"
+)
+
+# The engine is the core interface to the database
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal will be used to create an independent database session for each incoming web request
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base is the foundational class we will inherit from to create our database models
+Base = declarative_base()
