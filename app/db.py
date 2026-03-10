9a1b28af31165542d92eefb20385956992fa6031
ArchieAbr <archiepabraham@gmail.com>
2026-03-10 17:16:08 +0000

    fixed SQLAlchemy 2.0 'declarative_base' deprecation warning

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Fetch the database URL from the environment variables set in docker-compose.yml
