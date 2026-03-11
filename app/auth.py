0e8e561e84bccdf8da1e53f2723c49137cd7e9b3
ArchieAbr <archiepabraham@gmail.com>
2026-03-11 17:38:03 +0000

    Remove API key authorisation button from Swagger UI

--- app/auth.py
+++ app/auth.py
@@ -1,18 +1,14 @@
 import os
-from fastapi import Security, status
-from fastapi.security.api_key import APIKeyHeader
-from .exceptions import CustomAPIException  # Import it here
+from fastapi import Header, status
+from .exceptions import CustomAPIException
 
-API_KEY_NAME = "X-API-Key"
-api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
-
-async def verify_api_key(api_key: str = Security(api_key_header)):
+async def verify_api_key(x_api_key: str = Header(None, alias="X-API-Key", include_in_schema=False)):
     expected_key = os.getenv("API_KEY")
     
-    if not api_key or api_key != expected_key:
+    if not x_api_key or x_api_key != expected_key:
         raise CustomAPIException(
             name="AuthenticationError",
             detail="Invalid or missing API Key",
             status_code=status.HTTP_403_FORBIDDEN
         )
-    return api_key
+    return x_api_key
